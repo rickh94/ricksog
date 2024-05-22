ebd157695b3bc58f015b29fe167c963a44f1a60b
Rick Henry <rickhenry@rickhenry.dev>
2024-05-22 00:21:59 -0400

    update go practice url
    Signed-off-by: Rick Henry <rickhenry@rickhenry.dev>

--- src/app/practicebetter/opengraph-image.tsx
+++ src/app/practicebetter/opengraph-image.tsx
@@ -39,7 +39,7 @@ export default async function Image() {
             marginBottom: -64,
           }}
         >
-          practice.rickhenry.studio
+          musicpracticing.com
         </div>
       </Wrapper>
     ),
